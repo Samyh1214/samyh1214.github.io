db1f5803a0c944418dd6aeb39b818b0138ba7139
Samuel Fehrm <samuel.fehrm@yh.nackademin.se>
2025-12-26 12:50:34 +0100

    general fixes + read-me

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 document.getElementById("cv-button").addEventListener("click", function () {
   const link = document.createElement("a");
-  link.href = "https://drive.google.com/file/d/1-x8mfiRor7w8NNTBqSk6nKZHo74jcFGE/view?usp=sharing";
+  link.href = "/JS/CV-Samuel-Fehrm.pdf";
   link.download = "CV-Samuel-Fehrm.pdf";
   document.body.appendChild(link);
   link.click();
