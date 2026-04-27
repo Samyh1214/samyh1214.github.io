9b4d7dd52acf5bdc2d8751b451d7b85164461a86
Samuel Fehrm <samuel.fehrm@yh.nackademin.se>
2026-04-27 19:42:57 +0200

    cv fix url

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,8 @@
 document.getElementById("cv-button").addEventListener("click", function () {
   const link = document.createElement("a");
-  link.href = "/JS/CV-Samuel-Fehrm.pdf";
+  link.href = "CV-Samuel-Fehrm.pdf";
   link.download = "CV-Samuel-Fehrm.pdf";
+  link.type = "application/pdf";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
